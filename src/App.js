1f504347f3b54935af8b1fa2c3cd15247047f0b1
Karthik Swamy <getspk@gmail.com>
2024-07-21 15:05:27 -0500

    Week1 Assignment complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { CAMPSITES } from "./app/shared/CAMPSITES";
+import CampsitesList from "./features/campsites/CampsitesList";
 
 function App() {
-  return <div className="App">hello nucamp!</div>;
+  return (
+    <div className="App">
+      <Header />
+      <CampsitesList />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
